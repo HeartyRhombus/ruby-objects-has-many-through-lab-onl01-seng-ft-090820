51dedf24664eb9a1c1b4266a461baa90c84c48ae
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-23 19:11:37 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,12 @@
 class Appointment
+  attr_accessor :date, :day_of_week
 
+  @@all = []
 
-
+  def initialize(date, day_of_week)
+    @date = date
+    @day_of_week = day_of_week
+    "#{day_of_week}, #{date}"
+    @@all << self
+  end
 end
