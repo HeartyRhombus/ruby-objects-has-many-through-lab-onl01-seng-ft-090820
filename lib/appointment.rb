cbe80ddf76615c520613bba2f4157595a5ebdbcc
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-23 19:13:36 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,9 @@
 class Appointment
-  attr_accessor :month, :date, :day_of_week
+  attr_accessor :date, :patient, :doctor
 
   @@all = []
 
-  def initialize(month, date, day_of_week)
+  def initialize(date)
     @month = month
     @date = date
     @day_of_week = day_of_week
